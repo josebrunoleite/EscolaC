dcc25faf9689f7a4219822c2f74cd59566f3a5cb
ricardo1334 <135781304+ricardo1334@users.noreply.github.com>
2023-09-24 18:05:30 -0300

    Ver 0.4.3
    -Correção de digitação em
    ESCOLAC/database/migrations/create_cursoalunos
    e ESCOLAC/database/migrations/create_provas_table

--- database/migrations/2023_09_23_021352_create_cursoalunos_table.php
+++ database/migrations/2023_09_23_021352_create_cursoalunos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('curs_alun', function (Blueprint $table) {
+        Schema::create('cursoalunos', function (Blueprint $table) {
             $table->id();
             $table->string('aluno');
             $table->string('curso');
--- database/migrations/2023_09_23_021515_create_provas_table.php
+++ database/migrations/2023_09_23_021515_create_provas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('prova_cursos', function (Blueprint $table) {
+        Schema::create('provas', function (Blueprint $table) {
             $table->id();
             $table->string('curso');
             $table->string('titulo');
